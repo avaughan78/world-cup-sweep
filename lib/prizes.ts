350451b23a868bea7b5deb24c8b712f0c78874d6
Andy Vaughan <avaughan78@gmail.com>
2026-05-26 11:59:27 +0100

    Team award

--- lib/prizes.ts
+++ lib/prizes.ts
@@ -65,7 +65,7 @@ export async function computePrizes(
   return [
     {
       slug: 'most_cards',
-      name: 'The Josip Simunic Award',
+      name: 'The Josip Simunic Team Award',
       description: 'Filthiest team (yellow + red cards)',
       icon: '🟨',
       current_team: topCardsTotal > 0 ? topCards!.team_name : null,
